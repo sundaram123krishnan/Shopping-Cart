5566bc4e6ab706b0131545aabdd7012ccc504940
sundaram123krishnan <krishnsundaram@gmail.com>
2023-02-28 22:03:23 +0530

    bugs fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,36 @@ import Data from "./Data";
 
 export default function App() {
   const card = Data.map((value) => {
-    return <Card src={value.src} hoverImg={value.hoverImg} />;
+    return (
+      <Card
+        src={value.src}
+        hoverImg={value.hoverImg}
+        description={value.description}
+        category={value.category}
+        rating={value.rating}
+        size={value.size}
+      />
+    );
   });
-  return <div className="grid grid-cols-4 justify-items-center">{card}</div>;
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-10 mt-20">
+      <h1 className="text-7xl font-bold">Welcome!</h1>
+      <div className="flex gap-2">
+        <button className="bg-white p-3 text-black text-lg font-bold rounded-3xl">
+          S
+        </button>
+        <button className="bg-white p-3 text-black text-lg font-bold rounded-3xl">
+          M
+        </button>
+        <button className="bg-white p-3 text-black text-lg font-bold rounded-3xl">
+          L
+        </button>
+        <button className="bg-white p-3 text-black text-lg font-bold rounded-3xl">
+          XL
+        </button>
+      </div>
+      <div className="grid grid-cols-4 justify-items-center">{card}</div>;
+    </div>
+  );
 }
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { MouseEvent } from "react";
+import icon from "./icon.png";
 
-export default function Card(props) {
-  function mouseHover(event) {
-    event.target.src = props.hoverImg;
+export interface card {
+  hoverImg: string;
+  src: string;
+  category: string;
+  size: string;
+  description: string;
+  rating: string;
+}
+
+export default function Card(props: card) {
+  const item = {
+    hoverImg: props.hoverImg,
+    src: props.src,
+    category: props.category,
+    size: props.size,
+    description: props.description,
+    rating: props.rating,
+  } as card;
+
+  function mouseHover(e: React.SyntheticEvent<HTMLImageElement, Event>) {
+    (e.target as HTMLImageElement).src = props.hoverImg;
   }
-  function mouseLeave(event) {
-    event.target.src = props.src;
+  function mouseLeave(e: React.SyntheticEvent<HTMLImageElement, Event>) {
+    (e.target as HTMLImageElement).src = props.src;
   }
+  
+
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
@@ -19,10 +40,17 @@ export default function Card(props) {
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <div className="flex justify-between items-center font-bold text-2xl">
+          <h2 className="card-title">{props.category}</h2>
+          <h2>Size: {props.size}</h2>
+        </div>
+        <p>{props.description}</p>
+        <div className="card-actions justify-between items-center ">
+          <button className="bg-green-600 font-bold px-2 flex gap-2 items-center rounded-lg">
+            <img src={icon} alt="" className="h-5" />
+            {props.rating}
+          </button>
+          <button className="btn btn-primary">Add to Cart</button>
         </div>
       </div>
     </div>
