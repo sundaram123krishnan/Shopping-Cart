07a04f66c47fcb62375380e8632e111ae58c9528
sundaram123krishnan <krishnsundaram@gmail.com>
2023-03-02 22:05:42 +0530

    sliding navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "./Card";
 import Data from "./Data";
 import card from "./Card";
+import TemporaryDrawer from "./expense";
 
 export default function App() {
   const card = Data.map((value) => {
@@ -93,36 +94,62 @@ export default function App() {
     }
   }
 
+  function handleAllSizes() {
+    setItems(
+      Data.map((value) => {
+        return (
+          <Card
+            src={value.src}
+            hoverImg={value.hoverImg}
+            description={value.description}
+            category={value.category}
+            rating={value.rating}
+            size={value.size}
+          />
+        );
+      })
+    );
+  }
+
   return (
-    <div className="flex flex-col justify-center items-center gap-10 mt-20">
-      <h1 className="text-7xl font-bold">Welcome!</h1>
-      <div className="flex gap-2">
-        <button
-          className="bg-white p-3 text-black text-lg font-bold rounded-3xl"
-          onClick={handleSmall}
-        >
-          S
-        </button>
-        <button
-          className="bg-white p-3 text-black text-lg font-bold rounded-3xl"
-          onClick={handleMedium}
-        >
-          M
-        </button>
-        <button
-          className="bg-white p-3 text-black text-lg font-bold rounded-3xl"
-          onClick={handleLarge}
-        >
-          L
-        </button>
-        <button
-          className="bg-white p-3 text-black text-lg font-bold rounded-3xl"
-          onClick={handleExtraLarge}
-        >
-          XL
-        </button>
+    <div className="flex justify-evenly">
+      <div className="flex flex-col justify-center items-center gap-10 mt-20">
+        <h1 className="text-7xl font-bold">Welcome!</h1>
+        <div className="flex gap-2">
+          <button
+            className="bg-white p-3 text-black text-lg font-bold rounded-3xl"
+            onClick={handleSmall}
+          >
+            S
+          </button>
+          <button
+            className="bg-white p-3 text-black text-lg font-bold rounded-3xl"
+            onClick={handleMedium}
+          >
+            M
+          </button>
+          <button
+            className="bg-white p-3 text-black text-lg font-bold rounded-3xl"
+            onClick={handleLarge}
+          >
+            L
+          </button>
+          <button
+            className="bg-white p-3 text-black text-lg font-bold rounded-3xl"
+            onClick={handleExtraLarge}
+          >
+            XL
+          </button>
+          <button
+            className="bg-white p-3 text-black text-lg font-bold rounded-3xl uppercase"
+            onClick={handleAllSizes}
+          >
+            All Sizes
+          </button>
+        </div>
+        <div className="grid grid-cols-4 justify-items-center">{items}</div>;
       </div>
-      <div className="grid grid-cols-4 justify-items-center">{items}</div>;
+      <TemporaryDrawer />
     </div>
   );
 }
---
+++ src/expense.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+
+type Anchor = "top" | "left" | "bottom" | "right";
+
+export default function TemporaryDrawer() {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    ></Box>
+  );
+
+  return (
+    <div className="mt-24">
+      {(["right"] as const).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>
+            <AddShoppingCartIcon />
+          </Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
